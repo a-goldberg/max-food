f89e0ebe030ed81b26bbd0e7808c7c6bb162ec50
Adam Goldberg <a-goldberg@users.noreply.github.com>
2026-05-18 17:03:02 -0500

    minor food additions + server config adjustment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,12 @@ module.exports = {
     {
       name: "whoa-food",
       script: "server.js",
+      instances: 1,
+      autorestart: true,
       watch: ["server.js", "views", "public", "data"],
       ignore_watch: ["node_modules", ".git"],
+      max_memory_restart: "128M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
       env: {
         NODE_ENV: "production",
         PORT: 3001,
