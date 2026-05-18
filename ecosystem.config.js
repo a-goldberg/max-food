6eb8e3646aae8ca3374bd1ed960dfac54ca86bdb
Adam Goldberg <a-goldberg@users.noreply.github.com>
2026-05-18 13:25:29 -0500

    server name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "whoa-slow-go",
+      name: "whoa-food",
       script: "server.js",
       watch: ["server.js", "views", "public", "data"],
       ignore_watch: ["node_modules", ".git"],
